cc199dd5423c0568983c341194984b9b47534ac7
Denesjakab <denisjakabjakab@gmail.com>
2024-09-27 11:36:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +6,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+function generarExcusa() {
+  let quien = ["Mi madre", "Mi gato", "La vecina", "El cocodrilo"];
+  let accion = [
+    "rompió",
+    "arañó",
+    "mojó",
+    "descuartizó",
+    "pisoteó",
+    "robó",
+    "manipuló",
+    "se comió"
+  ];
+  let que = [
+    "el coche",
+    "mis deberes",
+    "mi cara",
+    "la ventana",
+    "mi regalo",
+    "el puzzle",
+    "el movil",
+    "las patatas"
+  ];
+  let cuando = [
+    "ayer",
+    "ahora mismo",
+    "cuando estaba de camino",
+    "hace dos dias",
+    "la semana pasada",
+    "pues no se"
+  ];
+  const persona = quien[Math.floor(Math.random() * quien.length)];
+  const acto = accion[Math.floor(Math.random() * accion.length)];
+  const objeto = que[Math.floor(Math.random() * que.length)];
+  const tiempo = cuando[Math.floor(Math.random() * cuando.length)];
+
+  const respuestaFinal = `${persona} ${acto} ${objeto} ${tiempo}`;
+  document.getElementById("resultado").innerHTML = respuestaFinal;
+}
+document.getElementById("Generarexcusa").addEventListener("click", () => {
+  generarExcusa();
+});
